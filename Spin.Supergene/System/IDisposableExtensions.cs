d4b6a6aff81ac4b4cb1cd2774f7643d20a7a0344
Robert Bouillon <robert_bouillon@hotmail.com>
2021-12-22 20:04:37 -0500

    Update to .NET 6

--- Spin.Supergene/System/IDisposableExtensions.cs
+++ Spin.Supergene/System/IDisposableExtensions.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace System
 {
   public static class IDisposableExtensions
   {
+    public static async Task DisposeAfterAsync<TIn>(this TIn disposable, Func<TIn, Task> action) where TIn : IDisposable
+    {
+      using(disposable)
+        await action(disposable);
+    }
+
+    public static async Task<TOut> DisposeAfterAsync<TIn, TOut>(this TIn disposable, Func<TIn, Task<TOut>> func) where TIn : IDisposable
+    {
+      using(disposable)
+        return await func(disposable);
+    }
+
     public static void DisposeAfter<TIn>(this TIn disposable, Action<TIn> action) where TIn : IDisposable
     {
       using (disposable)
