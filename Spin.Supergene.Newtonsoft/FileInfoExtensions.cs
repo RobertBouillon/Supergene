7f444ef7220d96b11bdc995ffd7fdefe3ffd1c58
Robert Bouillon <robert_bouillon@hotmail.com>
2023-05-16 20:15:20 -0400

    Properly flush JSON write

--- Spin.Supergene.Newtonsoft/FileInfoExtensions.cs
+++ Spin.Supergene.Newtonsoft/FileInfoExtensions.cs
@@ -7,8 +7,15 @@ public static class JsonFileInfoExtensions
   public static void WriteAllJson(this FileInfo file, Action<JsonWriter> json)
   {
     using (var fs = file.OpenWrite())
-    using (var sw = new StreamWriter(fs))
+    using (var buffer = new BufferedStream(fs))
+    using (var sw = new StreamWriter(buffer))
     using (var writer = new JsonTextWriter(sw))
+    {
       json(writer);
+      writer.Flush();
+      sw.Flush();
+      buffer.Flush();
+      fs.Flush();
+    }
   }
 }
