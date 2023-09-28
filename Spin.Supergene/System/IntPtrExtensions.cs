084e7009b193a5a82115a6f09ea4be8ee7e6f437
Robert Bouillon <robert_bouillon@hotmail.com>
2023-09-28 12:45:57 -0400

    IntPtr extensions

---
+++ Spin.Supergene/System/IntPtrExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace System;
+public static class IntPtrExtensions
+{
+  public static IntPtr Add(this IntPtr pointer, long offset) => new IntPtr(pointer.ToInt64() + offset);
+  public static IntPtr Add(this IntPtr pointer, int offset) => new IntPtr(pointer.ToInt64() + offset);
+}
